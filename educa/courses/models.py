3ba7cedaf0e280028aa8f8d3e9c43efce047d38e
YaGaskoin <32508888+YaGaskoin@users.noreply.github.com>
2020-05-06 16:41:36 +0300

    extends positive integer login/logout  templates mixins

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 # Create your models here.
 
@@ -36,6 +37,13 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
+
+    def __str__(self):
+        return '{}. {}'.format(self.order, self.title)
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
@@ -44,6 +52,10 @@ class Content(models.Model):
                                      limit_choices_to={'model__in': ('text', 'video', 'image', 'file')})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields='module')
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
